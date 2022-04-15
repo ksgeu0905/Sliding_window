53f6ca7d851106c08f32e94b326b871fbdbeec6d
Kartikya Singhal <Kartikyasinghalgeu@gmail.com>
2022-04-16 01:14:46 +0530

    Sliding Window

---
+++ maximum_subarray_Element_of_size_k.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+void cal_max_element(int arr[],vector<int>&ans,int n,int k){
+    int i=0,j=k-1;
+    list<int> l;
+    for(int z=i;z<=j;z++){
+        if(l.empty())
+            l.push_back(arr[z]);
+        else{
+            while(!l.empty() && arr[z]>l.front())
+                l.pop_front();
+            l.push_back(arr[z]);
+        }
+    }
+    while(j<n){
+        ans.push_back(l.front());
+        if(arr[i]==l.front())
+            l.pop_front();
+        i++;j++;
+        if(j!=n){
+                while((!l.empty()) && arr[j]>l.front()){
+                        l.pop_front();
+                }
+                l.push_back(arr[j]);
+            }
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int k;
+    cout<<"Enter size of window: ";
+    cin>>k;
+    vector<int>ans;
+    cal_max_element(arr,ans,n,k);
+    for(auto i:ans){
+        cout<<i<<" ";
+    }
+        return 0;
+}
